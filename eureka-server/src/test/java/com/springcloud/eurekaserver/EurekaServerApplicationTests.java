f8d17eac7787f898e6b6672673c00be8d4ef9ead
ijhwangitall <ijhwang@itall.com>
2025-04-23 11:36:28 +0900

    eureka 서버 셋팅

--- eureka-server/src/test/java/com/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springcloud;
+package com.springcloud.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcloudApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
